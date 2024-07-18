41d05dbac1ef824c800ce53a209633b003a7c785
Nathaniel Tan <nathaniel.tan@student.unsw.edu.au>
2024-07-18 23:08:46 +1000

    test button component import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// Dummy button to test imports
+import SomeButton from './components/SomeButton'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
+        {/* Testing component import */}
+        <SomeButton/>
+
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
---
+++ frontend/src/components/SomeButton.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+// This component is a test component, don't worry about this
+const SomeButton = () => {
+  const [count, setCount] = useState(0);
+
+//   let c = 0;
+  const increase = (c: number) => {
+    console.log(c + 1);
+    c++;
+  }
+
+  return (
+    <button onClick={() => increase(count)}>SomeButton {count}</button>
+  )
+}
+
+export default SomeButton
